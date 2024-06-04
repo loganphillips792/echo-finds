866ed454c6efc4090a17d0a7b97b98c8a7e6d5b6
loganphillips792 <loganphillips792@gmail.com>
2024-06-03 21:39:28 -0400

    wip

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Echo is a nymph known for her voice, symbolizing repeated alerts
 
+This app tracks the release of specific products, notifying you when a new product is announced and when it is released. Once a product is released, you will receive a separate email with details on how to obtain it.
+
 # Running locally
 
 ## Installing Python depdencies
@@ -12,9 +14,19 @@ Echo is a nymph known for her voice, symbolizing repeated alerts
 4. ```~/Desktop/EchoFinds/bin/python3 -m pip install --upgrade pip```
 5. ```pip install -r requirements.txt```
 
+## Chrome Driver
+
+The Selenium library in Python requires a driver depending on the web browser you are using. In this case, we are using Chrome, so go to this site: https://googlechromelabs.github.io/chrome-for-testing/ and then when the download completes, drag and drop the executable into the root of this folder. The version of chromedriver you need to download will depend on the version of the Chrome browser you are using.
+
 ## Running application
 
 1. Go to project root
 2. Create environment file (.env)
 3. Enable virtual environment
 4. python3 main.py
+
+# Support
+
+These are the sites that are currently supported:
+
+- [Owala Color Drop](https://owalalife.com/pages/color-drop)
---
+++ main.py
@@ -0,0 +1,149 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import sqlite3
+import os
+from dotenv import load_dotenv
+import logging
+import logging.config
+
+load_dotenv()
+
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        },
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': 'app.log',
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        '': {  # root logger
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'my_module': {  # a specific logger for 'my_module'
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+    }
+}
+
+# Apply the logging configuration
+logging.config.dictConfig(LOGGING_CONFIG)
+
+# Create a logger
+logger = logging.getLogger('my_module')
+
+# use logger. If one is not specified, it will default to the root one
+logging.info('starting script')
+
+def create_database():
+    conn = sqlite3.connect('water_bottles.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS bottles
+                (name TEXT, release_date TEXT)''')
+    conn.commit()
+    conn.close()
+
+def save_to_database(name, release_date):
+    conn = sqlite3.connect('water_bottles.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO bottles (name, release_date) VALUES (?, ?)", (name, release_date))
+    conn.commit()
+    conn.close()
+
+def scrape_website(url):
+    # print(f"Scraping website {url}")
+    logging.info(f'scraping {url}')
+    
+    # Set up Selenium WebDriver
+    options = Options()
+    # options.headless = True  # Run in headless mode (without opening a browser window)
+    # options.add_argument('--headless')
+    options.binary_location = os.getenv('CHROME_BROWSER_PATH')  # Path to Chrome binary on macOS
+    service = Service(executable_path='./chromedriver', log_path='NUL')  # Replace with your WebDriver's path
+    driver = webdriver.Chrome(service=service, options=options)
+    
+    driver.get(url)
+
+    # Wait until the elements with class 'color-drop__blocks' are present. This is the grid of products
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'color-drop__blocks'))
+    )
+    
+    # Wait for the elements to be present
+    # wait = WebDriverWait(driver, 10)
+
+    product_containers = driver.find_elements(By.CLASS_NAME,'product__text-container')
+
+    product_details = []
+    for container in product_containers:
+        product_name_element = container.find_element(By.CLASS_NAME, 'product__name')
+        product_name = product_name_element.text if product_name_element else 'N/A'
+        logging.info(f'product name is {product_name}')
+
+
+    # bottle_elements = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.product__container')))  # Update this
+    # release_elements = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.some-class-for-date')))  # Update this
+
+    # for bottle in bottle_elements:
+    #         print("hi")
+    #         print(bottle)
+
+    # for bottle, release in zip(bottle_elements, release_elements):
+    #     name = bottle.text.strip()
+    #     release_date = release.text.strip()
+    #     save_to_database(name, release_date)
+    
+    driver.close()
+    
+    
+    
+    
+    
+    
+    
+    
+    # page = requests.get(url, timeout=5)
+    
+    # soup = BeautifulSoup(page.content, 'html.parser')
+    # # bottle_elements = soup.select('.product__container')
+    # print(soup.prettify)
+
+    # bottle_elements = soup.find_all("div", class_="")
+
+    
+
+def main():
+    create_database()
+
+    if os.getenv('TRACK_OWALA') == 'True':
+        url = os.getenv('OWALA_URL')
+        if not url:
+            raise ValueError("OWALA_URL environment variable is not set.")
+        scrape_website(url)
+
+    
+if __name__ == '__main__':
+    main()
